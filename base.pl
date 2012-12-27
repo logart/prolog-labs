e61f2a5758ced43783310df830591c85359a60e9
artem <logart2007@gmail.com>
2012-12-27 16:56:41 -0500

    final version of program and base added

---
+++ base.pl
@@ -0,0 +1,120 @@
+question(c,'Are you cool hacker?').
+question(plus, 'Do you like add operation?').
+question(increment,'Do you like incrementing?').
+question(decrement,'Do you like decrementing?').
+question(sharp, 'Do you like high sounds?').
+question(exotic,'Do you like tropics?').
+question(alphabet, 'Do you like ABC song?').
+question(algol, 'Do you like sixtieth?').
+question(scripting,'Do you like slow programs?').
+question(apple,'Do you like Steve?').
+question(one_letter,'Are you minimalist?').
+question(regexp,'Do you like crying with blood?').
+question(stupid,'Do you want to destroy your brain?').
+question(beta,'Are you hate releases?').
+question(food,'Do you like eating?').
+question(color,'Do you like colours?').
+question(horrible,'Are you terorist?').
+question(professional,'Are you professional?').
+question(math,'Are you scientist?').
+question(cat,'Do you like cat?').
+question(porshe,'Do you like porshe?').
+question('Norris','Do you like Chuck?').
+question(lisp,'Are you hate side effects?').
+question(school,'Are you schoolboy?').
+question(logic, 'Have you logic?').
+
+like(c,'C--').
+like(c,'C++').
+like(c,'C#').
+like(c,'C shell').
+
+like(plus, 'A+').
+
+like(increment, 'A++').
+like(increment,'C++').
+
+like(decrement,'C--').
+
+
+like(sharp, 'A#').
+like(sharp, 'C#').
+
+like(exotic, 'A-0').
+like(exotic, 'ABAP').
+like(exotic, 'ABLE').
+like(exotic, 'ABSET').
+like(exotic, 'ABSYS').
+like(exotic, 'ACC').
+like(exotic,'Accent').
+like(exotic,'Ace DASL').
+like(exotic,'ACT-III').
+like(exotic,'AMOS').
+like(exotic,'ARC').
+like(exotic,'BCPL').
+like(exotic,'AutoHotkey').
+like(exotic,'AutoIt').
+like(exotic,'BPEL').
+like(exotic,'BYOND').
+like(exotic,'BLISS').
+like(exotic,'BUGSYS').
+like(exotic,'Bigwig').
+like(exotic,'AWL').
+like(exotic,'bc').
+
+like(alphabet, 'ABC').
+like(alphabet, 'ABC ALGOL').
+
+like(algol, 'ABC ALGOL').
+
+like(scripting,'ActionScript').
+like(scripting,'AppleScript').
+like(scripting,'Bash').
+like(scripting,'C shell').
+like(scripting,'BeanShell').
+like(scripting,'Bourne shell').
+like(scripting,'ECMAScript').
+
+like(apple,'AppleScript').
+
+like(one_letter,'Amiga E').
+like(one_letter,'C--').
+like(one_letter,'C++').
+like(one_letter,'C#').
+like(one_letter,'C shell').
+
+like(regexp,'AWK').
+
+like(stupid,'BASIC').
+like(stupid,'BASIC-256').
+like(stupid,'Blitz Basic').
+
+like(beta,'BETA').
+
+like(food,'Bistro').
+
+like(color,'Blue').
+
+like(horrible,'Boo').
+like(horrible,'Bourne shell').
+like(horrible,'Brainfuck').
+
+like(professional,'BuildProfessional').
+
+
+like(math,'Caml').
+like(math,'Erlang').
+
+like(cat,'Cat').
+
+like(porshe,'Cayenne').
+
+like('Norris','ChucK').
+
+like(lisp,'Clojure').
+like(lisp,'Common Lisp').
+like(lisp,'Emacs Lisp').
+
+like(school,'Delphi').
+
+like(logic, 'prolog').
--- lab4.pl
+++ lab4.pl
@@ -1,17 +1,5 @@
 :- dynamic like/2.
 :- dynamic notlike/2.
-%% like('exotic', 'C').
-%% like('exotic', 'D').
-%% like('increment', 'C++').
-%% like('increment', 'D').
-%% like('OO', 'A').
-
-%% question('exotic', 'Do you like exotic languages?').
-%% question('increment', 'Do you like incrementing?').
-%% question('OO', 'Do you like OOP?').
-
-
-%% notlike(A,B) :- \+ like(A,B).
 
 retractList([]).
 retractList([H|T]) :- retractall(like(_,H)), retractList(T).
@@ -30,10 +18,6 @@ say([R|T]) :- write('The best choise for you will be one of the following langua
 answer([]) :- findall(Next, like(_, Next), L), uniq(L, R), say(R);say([]).
 ask(QL) :- findall(Q,(like(Q,_),nonmember(Q,QL)), [QHead|QTail]),
                 question(QHead, Text),
-                %% write([QHead|QTail]),
-                %% nl,
-                %% write(QL),
-                %% nl,
                 write(Text),
                 insert(QHead,QL,NewQL),
                 nl,
@@ -41,12 +25,9 @@ ask(QL) :- findall(Q,(like(Q,_),nonmember(Q,QL)), [QHead|QTail]),
                 ( (Response == yes ; Response == y) -> 
                     findall(Next,like(QHead, Next), L),
                     findall(Next1,(like(_, Next1),nonmember(Next1, L)), Ruha),
-                    %% write(L),nl,
-                    %% write(Ruha),nl,
                     retractList(Ruha);
                     
                     findall(Next,like(QHead, Next), L),
-                    %% write(L),nl,
                     retractList(L)
                 ),
                 ask(NewQL);answer([]).
@@ -54,25 +35,14 @@ ask(QL) :- findall(Q,(like(Q,_),nonmember(Q,QL)), [QHead|QTail]),
 main :-
         nl,
         write('Choose action:\n'),
-        write('1. save current state\n'),
-        write('2. find element\n'),
-        write('3. add element\n'),
-        write('4. remove element\n'),
-        write('5. exit\n'),
+        write('1. select prograaming language according to your personality\n'),
+        write('2. exit\n'),
         write('>>> '), 
         readln([X]),
         switch(X) ; main.
  
-switch(1):- qsave_program('lab2.out',[class(development),op(save),autoload(true),goal(init)]), main.
+switch(1):- consult('base.pl'), ask([]), main.
  
-switch(2):- ask([]), main.
- 
-switch(3):- write('Type parent\'s name: '), read_line_pls(P),
-                        write('Type child\'s name: '), read_line_pls(C),
-                        assert(parent(P,C)), main.
- 
-switch(4):- write('Type element\'s name: '), read_line_pls(A), retractall(parent(A,_)),retractall(parent(_,A)), main.
-       
-switch(5):-     write('Bye.\n'), halt, halt.
+switch(2):-     write('Bye.\n'), halt, halt.
 
 init :- [library(http/dcg_basics),library(dialect/sicstus)], consult('base.pl'), main.
